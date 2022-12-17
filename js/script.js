ca208a3f25ac2746a51321d8bc4dc0a548e7723f
AndreaGarofalo <andreasjobs94@outlook.it>
2022-12-17 19:10:31 +0100

    finished base exerciese

--- js/script.js
+++ js/script.js
@@ -17,3 +17,50 @@ console.log("JS OK");
 // - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+// 1 prendo gli elementi dal dom
+// 2 creo un event listener collegato al bottone
+// 3 creo un loop che crei le caselle con dentro il rispettivo numero
+// 4 creo un event listener che aggiunga la classe clicked alle varie caselle
+// 5 ci aggiungo il console log con il numero della casella
+
+// 3.5 creo una funzione che crei le caselle
+const createCell = (content) => {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.append(content);
+  return cell;
+};
+
+// 0.5 preparo le impostazioni iniziali
+const rows = 10;
+const cols = 10;
+const totalCells = rows * cols;
+
+// 1 prendo gli elementi dal dom
+const container = document.getElementById("custom-container");
+const button = document.getElementById("button");
+const title = document.getElementById("title");
+const pippo = document.getElementById("pippo");
+
+// 2 creo un event listener collegato al bottone
+button.addEventListener("click", function () {
+  const prevContainer = document.getElementById("grid");
+  if (prevContainer) {
+    prevContainer.remove();
+  }
+  const grid = document.createElement("div");
+  grid.setAttribute("id", "grid");
+  // 3 creo un loop che crei le caselle
+  for (let i = 1; i <= totalCells; i++) {
+    const cell = createCell(i);
+    // 4 creo un event listener che aggiunga la classe clicked alle varie caselle
+    cell.addEventListener("click", () => {
+      cell.classList.toggle("clicked");
+      console.log(i);
+    });
+    grid.appendChild(cell);
+    title && title.remove();
+    container.appendChild(grid);
+  }
+});
